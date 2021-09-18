c28b18a59b298084f5b7cf0fdb5a3a732be4dccc
Austin Rhoads <austinrrhoads@ymail.com>
2021-09-17 20:50:51 -0500

    app file import components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/Home.js';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
+
+class App extends Component{
+
+
+
+
+
+  componentDidMount(){
+
+
+    var sections = document.querySelectorAll('section');
+   
+   
+    this.set_current_section(sections)
+ 
+
+    window.addEventListener('scroll', ()=> {
+
+      const navbar = document.getElementById('nav-bar');
+
+      if(window.pageYOffset > (window.innerHeight + (window.innerHeight * 0.2)) && !navbar.classList.contains('fixed')){
+      
+         navbar.classList.add('fixed')
+      
+      } else if (window.pageYOffset < window.innerHeight && navbar.classList.contains('fixed')){
+
+          navbar.classList.remove('fixed')
+      
+      }
+
+
+      this.set_current_section(sections)
+
+      
+
+
+    })
+
+
+  }
+
+  set_current_section = (sections) => {
+
+    let old_pos = document.querySelector('.lit') ? document.querySelector('.lit').id: null
+
+    for(let x = 0; x < sections.length; x++){
+      if(window.pageYOffset > sections[x].offsetTop - 30  && window.pageYOffset < sections[x].offsetTop + (sections[x].offsetHeight/2)){
+        let pos = `link-${sections[x].id}`
+       
+        if(old_pos !== pos){
+          console.log(old_pos)
+          let old_link = document.querySelector('.lit')
+          
+          if(old_link){old_link.classList.remove('lit')}
+          let current_link = document.getElementById(pos);
+          current_link.classList.add('lit')
+         
+          
+        }
+
+      }
+    }
+  }
+
+
+  scroll_to = (dest) => {
+
+    let topOfDestinationElement = document.getElementById(dest).offsetTop
+
+    window.scrollTo(0, topOfDestinationElement)
+    this.hide_responsive()
+
+  }
+
+  hide_responsive = () => {
+    var navbar = document.getElementById('nav-bar');
+    if(navbar.classList.contains('responsive')){
+        navbar.classList.remove('responsive')
+    }
+}
+
+
+
+  render (){
+    return(
+      <div className="App">
+        <Home scroll_to={this.scroll_to} />
+        <Navbar scroll_to={this.scroll_to}  />
+        <About />
+        <Portfolio />
+        <Blog />
+        <Contact />
+      </div>
+    )
+ 
+  };
 }
 
 export default App;
