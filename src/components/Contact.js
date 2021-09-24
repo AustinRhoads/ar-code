b471af6c71c4d78a98c8f6298c62ea7279208c7e
Austin Rhoads <austinrrhoads@ymail.com>
2021-09-24 16:11:53 -0500

    fixed head icon and contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react' 
+import white_git from '../images/git64white.png' 
+import linkedin_blue from '../images/LinkedIn-Blue-14@2x.png'
 
 class Contact extends Component {
     render(){
         return(
             <section id="contact-div">
                 <div id="contact-header">CONTACT ME</div>
+                <div id="contacts_list">
+                    <div className="contact-element" id="contact-email-div">Email: Austin.rhoads.code@gmail.com</div>
+                    <div className="contact-element" id="contact-linkedin-div icon-element"><a href="https://www.linkedin.com/in/austinrhoads-code/"><img alt ="" src={linkedin_blue} /></a></div>
+                    <div className="contact-element" id="contact-github-div icon-element"><a href ="https://github.com/AustinRhoads" ><img alt="" src={white_git} /></a></div>
+                </div>
             </section>
         )
     }
