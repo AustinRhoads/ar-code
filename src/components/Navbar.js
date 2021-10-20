5c14ee8902d61acf698a8c8f8a96e63086f55d2c
Austin Rhoads <austinrrhoads@ymail.com>
2021-10-20 11:43:01 -0500

    navbar underline link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,11 +39,11 @@ class Navbar extends Component {
         return(
             <div id="nav-bar" className="nav-bar">
                 
-                    <div id="link-home" className="nav-link"onClick={() => this.props.scroll_to("home")} onMouseEnter={(el) => this.litUp(el)} onMouseLeave={(el) => this.litUp(el)}>HOME</div>
-                    <div id="link-about-all" className="nav-link"onClick={() => this.props.scroll_to("about-all")}  onMouseEnter={(el) => this.litUp(el)} onMouseLeave={(el) => this.litUp(el)} >ABOUT</div>
-                    <div id="link-portfolio-div" className="nav-link"onClick={() => this.props.scroll_to("portfolio-div")}  onMouseEnter={(el) => this.litUp(el)} onMouseLeave={(el) => this.litUp(el)} >PROJECTS</div>
-                    <div id="link-blog-div" className="nav-link"onClick={() => this.props.scroll_to("blog-div")}  onMouseEnter={(el) => this.litUp(el)} onMouseLeave={(el) => this.litUp(el)} >BLOG</div>
-                    <div id="link-contact-div" className="nav-link"onClick={() => this.props.scroll_to("contact-div")}  onMouseEnter={(el) => this.litUp(el)} onMouseLeave={(el) => this.litUp(el)} >CONTACT</div>
+                    <div id="link-home" className="nav-link"onClick={() => this.props.scroll_to("home")}>HOME<div className="line"></div></div>
+                    <div id="link-about-all" className="nav-link"onClick={() => this.props.scroll_to("about-all")} >ABOUT<div className="line"></div></div>
+                    <div id="link-portfolio-div" className="nav-link"onClick={() => this.props.scroll_to("portfolio-div")}>PROJECTS<div className="line"></div></div>
+                    <div id="link-blog-div" className="nav-link"onClick={() => this.props.scroll_to("blog-div")} >BLOG<div className="line"></div></div>
+                    <div id="link-contact-div" className="nav-link"onClick={() => this.props.scroll_to("contact-div")}  >CONTACT<div className="line"></div></div>
                 
                 <i id="nav-icon" onClick={() => this.toggle_responsive()}>
                     <div id="nav-icon-line-1"></div>
